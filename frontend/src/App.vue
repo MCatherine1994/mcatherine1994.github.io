dc062b4ed2ca15534285ffd0c88f0c3d7acff5b3
Catherine Meng <mcatherine1994@gmail.com>
2025-03-22 23:01:55 -0700

    feat: added profile sidebar placeholder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Splitter from 'primevue/splitter'
+import SplitterPanel from 'primevue/splitterpanel'
+
+import SideBar from '@/components/SideBar.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <Splitter style="height: 100vh">
+      <SplitterPanel
+        class="flex align-items-center justify-content-center sidebar"
+        :size="17"
+        :minSize="10"
+      >
+        <SideBar />
+      </SplitterPanel>
+      <SplitterPanel class="flex align-items-center justify-content-center" :size="83">
+        Panel 2
+      </SplitterPanel>
+    </Splitter>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.sidebar {
+  padding: 0.625rem;
+  background-color: var(--p-purple-100);
 }
 </style>
---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Avatar from 'primevue/avatar'
+</script>
+
+<template>
+  <div class="sidebar">
+    <Avatar image="/da.png" class="mr-2 profile-img" size="xlarge" shape="circle" />
+    <h2 id="profile-name">Catherine Meng</h2>
+    <h4 id="profile-title">
+      Full Stack Developer<br />
+      Software Developer <br />
+      Machine Learning Engineer
+    </h4>
+    <p>Hello world</p>
+  </div>
+</template>
+
+<style scoped>
+.profile-img {
+  height: 12.5rem;
+  width: 12.6rem;
+}
+</style>
